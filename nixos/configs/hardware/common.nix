6367c1ae7a0239e6ca25e86e4117ae64ace49143
halva <eli@halvacoffee.fyi>
2024-12-29 22:14:29 +0300

    rework!

--- nixos/configs/hardware/common.nix
+++ nixos/configs/hardware/common.nix
@@ -1,35 +1,31 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./secrets.nix
-  ];
-
   # Nix settings
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.auto-optimise-store = true;
   nixpkgs.config.allowUnfree = true;
 
-  nix.gc = {
-    automatic = true;
-    dates = "14d";
-    randomizedDelaySec = "6h";
-    persistent = true;
-    options = "--delete-older-than 7d";
-  };
+  # nix.gc = {
+  #   automatic = true;
+  #   dates = "14d";
+  #   randomizedDelaySec = "6h";
+  #   persistent = true;
+  #   options = "--delete-older-than 7d";
+  # };
   
   # Use a different kernel
-  boot.kernelPackages = pkgs.linuxPackages_cachyos;
+  # boot.kernelPackages = pkgs.linuxPackages_cachyos;
 
 
   # Pretty boot!
-  boot.plymouth.enable = true;
-  boot.plymouth.theme = "bgrt";
-  boot.initrd.systemd.enable = true;
-  boot.initrd.verbose = false;
-  boot.consoleLogLevel = 0;
-  boot.kernelParams = [ "quiet" "udev.log_level=0" ];
-  console.earlySetup = true;  
+  # boot.plymouth.enable = true;
+  # boot.plymouth.theme = "bgrt";
+  # boot.initrd.systemd.enable = true;
+  # boot.initrd.verbose = false;
+  # boot.consoleLogLevel = 0;
+  # boot.kernelParams = [ "quiet" "udev.log_level=0" ];
+  # console.earlySetup = true;  
 
   # Networking settings
   time.timeZone = "Europe/Moscow";
@@ -44,17 +40,15 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
   hardware.firmware = [ pkgs.rtl8761b-firmware ];
-  hardware.xpadneo.enable = true;
 
   # User settings
   users.users.eli = {
     isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd"  ];
+    extraGroups = [ "networkmanager" "wheel" ];
   };
 
   # Virtualisation settings
-  virtualisation.docker.enable = true;
-  virtualisation.libvirtd.enable = true;
+  virtualisation.podman.enable = true;
 
   # Services and apps
   services.avahi.publish.enable = true;
@@ -115,6 +109,7 @@
   services.printing.enable = true;
   services.zerotierone = {
     enable = true;
+    joinNetworks = [ "93afae5963c40e46" ];
     localConf = { 
       settings = { 
         softwareUpdate = "disable";
